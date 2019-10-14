645cb0b0e3c8034363498f0e2c30e7ac0cce93cd
Unknown <jhut98@gmail.com>
2019-10-14 21:29:07 +0200

    Basic app

--- app/Teamscan/src/app/app-routing.module.ts
+++ app/Teamscan/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TodoComponent } from './todo/todo.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent, data: {title: 'Teamscan'} },
+  { path: 'todo', component: TodoComponent, data: {title: 'To Do'} }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app/Teamscan/src/app/app.component.ts
+++ app/Teamscan/src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { filter, map, shareReplay } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'Teamscan';
+  constructor( private breakpointObserver: BreakpointObserver, private titleService: Title, private router: Router, private activatedRoute: ActivatedRoute) {}
+  ngOnInit() {
+    this.router
+    .events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => {
+        const child = this.activatedRoute.firstChild;
+        if (child.snapshot.data['title']) {
+          return child.snapshot.data['title'];
+        }
+        return this.title;
+      })
+    ).subscribe((ttl: string) => {
+      this.titleService.setTitle(ttl);
+      this.title = ttl;
+      console.log("change title");
+    });
+  }
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  .pipe(
+    map(result => result.matches),
+    shareReplay()
+  );
 }
