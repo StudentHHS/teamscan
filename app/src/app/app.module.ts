6b910be294c14799ccc37f89d0f50e1e86ec64d0
Unknown <jhut98@gmail.com>
2019-10-16 21:23:38 +0200

    Navigatie en menu

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -20,13 +20,14 @@ import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [AppComponent, MenuComponent, TodoComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule,
     MatButtonModule, MatSidenavModule, MatSidenavModule, MatToolbarModule, MatCardModule,
-    MatGridListModule, MatMenuModule, MatIconModule
+    MatGridListModule, MatMenuModule, MatIconModule, MatListModule
   ],
   providers: [
     StatusBar,
