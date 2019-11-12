0bd0fcb4b28e6c53bcd60290c8fc0765021b918e
Unknown <jhut98@gmail.com>
2019-11-13 00:45:45 +0100

    Minimale veranderingen
    Errorteksten en zoekfunctie verbeterd in firstUse-scherm

--- app/src/app/login/register.component.ts
+++ app/src/app/login/register.component.ts
@@ -16,7 +16,7 @@ export interface OpleidingenGroup {
 export const _filter = (opt: string[], value: string): string[] => {
     const filterValue = value.toLowerCase();
   
-    return opt.filter(item => item.toLowerCase().indexOf(filterValue) === 0);
+    return opt.filter(item => item.toLowerCase().indexOf(filterValue) >= 0);
 };
   
 @Component({
