cba5fb7bee7fd4962b6fa80b97f5187d70fac3d3
Unknown <jhut98@gmail.com>
2019-11-25 17:13:44 +0100

    Teamleden toevoegen en tonen

--- app/src/app/teams/createteam/createteam.component.ts
+++ app/src/app/teams/createteam/createteam.component.ts
@@ -4,6 +4,8 @@ import { FormGroup, FormControl, FormBuilder, Validators, FormArray } from '@ang
 import { MatChipInputEvent, MatChipList } from '@angular/material/chips';
 import { HttpClient } from '@angular/common/http';
 import { AuthService } from '../../auth.service';
+import { ToastController } from '@ionic/angular';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-createteam',
@@ -22,19 +24,48 @@ export class CreateTeamComponent {
   readonly separatorKeysCodes: number[] = [ENTER, COMMA];
   public invalidEmailInput:Boolean = false;
 
-  constructor(private fb: FormBuilder, private authService: AuthService, private http: HttpClient) {}
+  constructor(private fb: FormBuilder, private location: Location, private authService: AuthService, private http: HttpClient, private toastController: ToastController) {}
 
   ngOnInit() {}
 
   createTeamForm: FormGroup = this.fb.group({
     naam: ['', Validators.required],
     beschrijving: ['', Validators.required],
-    teamleider: ['', Validators.required],
+    teamleider: ['', [Validators.required], [this.isValidUser.bind(this)]],
     teamleden: new FormArray([], [this.validateArrayNotEmpty])
   });
 
   get getteamleden() {
     return this.createTeamForm.get('teamleden') as FormArray;
+  }
+
+
+  isValidUser(c: FormControl) : Promise<any> {
+    return  new Promise<any>(
+      (resolve, reject) => {
+      if(this.validateEmail(c.value)) {
+        this.http.get(
+          'https://teamscan.ga/api/?function=getuseridbymail&token='+this.authService.token,
+          { headers: null, responseType: 'json', params: {mail: c.value} }
+        ).subscribe((data) => {
+          resolve(null);
+        },
+        error => {
+          if(error.status == 404) {
+            resolve({isValidUser: { valid: false } }); 
+          } else {
+            this.showToast("Verifiëren is eventjes niet gelukt", 1000);
+            console.log("error at data request", error);
+            reject();
+          }
+        });
+      } else {
+        resolve({isValidUser: { valid: false } });
+      }
+  });
+    ///if (c.value !== "jhut")
+    //  return {isValidUser: { valid: false } };
+    //return null;
   }
 
   //Check of er minimaal 2 emails zijn ingevuld.
@@ -78,14 +109,24 @@ export class CreateTeamComponent {
     console.log(this.createTeamForm.value);
   }
 
+  async showToast(text: string, duration: number) {
+    const toast = await this.toastController.create({
+      message: text,
+      duration: duration,
+    });
+    toast.present();
+  }
+
   onSubmit(formData: any){
     console.log(formData);
+    formData.teamledenstring = formData.teamleden.toString();
     if(this.createTeamForm.valid){
     this.http.get(
       'https://teamscan.ga/api/?function=createnewteam&token='+this.authService.token,
       { headers: null, responseType: 'json', params: formData })
       .subscribe(data => {
         console.log("send",data)
+        this.location.back();
       });
     }
   }
--- app/src/app/teams/team/team.component.ts
+++ app/src/app/teams/team/team.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { map } from 'rxjs/operators';
-import { AppComponent } from 'src/app/app.component';
+import { HttpClient } from '@angular/common/http';
 import { UtilityService } from 'src/app/utility.service';
+import { AuthService } from 'src/app/auth.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-team',
@@ -12,13 +12,46 @@ import { UtilityService } from 'src/app/utility.service';
 })
 
 export class TeamComponent {
-public id: string;
+  public id: string;
+  requestFailed;
+  public teamdata : Object = Array();
+
   constructor(
     private route: ActivatedRoute,
-    private us: UtilityService) {}
+    private us: UtilityService,
+    private http: HttpClient,
+    private authService: AuthService,
+    private toastController: ToastController) {}
 
   ngOnInit() {
     this.id = this.route.snapshot.paramMap.get('id');
     this.us.changeTitle(this.id);
- }
+    this.getData();
+  }
+
+  getData() {
+    this.http.get(
+        'https://teamscan.ga/api/?function=getteam&token='+this.authService.token,
+        { headers: null, responseType: 'json', params: {teamid: this.id} }
+      ).subscribe(data => {
+        console.log("resultaat");
+        console.log(data);
+        this.requestFailed = false;
+        this.teamdata = data;
+      },
+      error => {
+        this.showToast("De vragen konden niet worden ingeladen. Ben je nog verbonden?", 3000);
+        this.requestFailed = true;
+        console.log("error at data request", error);
+      }
+    );
+  }
+    
+  async showToast(text: string, duration: number) {
+    const toast = await this.toastController.create({
+      message: text,
+      duration: duration,
+    });
+    toast.present();
+  }
 }
