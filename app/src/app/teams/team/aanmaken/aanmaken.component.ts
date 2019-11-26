1add0e11c285dc5761846e1bfd203375f6118633
Charlie <charlie.vierling@hotmail.com>
2019-11-26 15:11:13 +0100

    Revert "Data kan nu worden verstuurd"
    This reverts commit f39ea3207780b98f401002e2c375a2f6d10d1d96.

--- app/src/app/teams/team/aanmaken/aanmaken.component.ts
+++ app/src/app/teams/team/aanmaken/aanmaken.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { FormGroup, FormControl, FormBuilder, Validators, FormArray } from '@angular/forms';
+
+import {MatDatepickerModule} from '@angular/material/datepicker';
+
 import { Location } from '@angular/common';
 import { ToastController } from '@ionic/angular';
 import { AuthService } from 'src/app/auth.service';
@@ -8,46 +10,40 @@ import { AuthService } from 'src/app/auth.service';
 @Component({
   selector: 'app-aanmaken',
   templateUrl: './aanmaken.component.html',
-  styleUrls: ['./aanmaken.component.css'],
+  styleUrls: ['./aanmaken.component.scss'],
 })
 export class AanmakenComponent implements OnInit {
   constructor(
-    private http: HttpClient,
+    private http: HttpClient, 
     private location: Location,
-    private toastController: ToastController,
-    private authService: AuthService,
-    private fb: FormBuilder,) {}
+    private toastController: ToastController, 
+    private authService: AuthService) {
+
+    }
 
   ngOnInit() {}
 
-  createTeamScanForm: FormGroup = this.fb.group({
-    naam: ['', Validators.required],
-    beginDatum: ['', Validators.required],
-    eindDatum: ['', Validators.required]
-  });
-
-  onSubmit(formData: any){
-    console.log(formData);
-    console.log(this.createTeamScanForm.valid);
-    if(this.createTeamScanForm.valid){
-    this.http.get(
-      'https://teamscan.ga/api/?function=createnewteamscan&token='+this.authService.token,
-      { headers: null, responseType: 'json', params: formData })
-      .subscribe(data => {
-        console.log("send",data);
-      },
-      error => {
-        this.showToast("We konden de gegevens niet opslaan. Ben je nog verbonden?", 3000);
-        console.log("error at data request", error);
-        }
-      );
-    }
+  createTeamscan(formData: any){
+    // console.log(formData);
+    //     this.http.get(
+    //       'https://teamscan.ga/api/?function=saveuserinfo&token='+this.authService.token,
+    //       { headers: null, responseType: 'json', params: formData }
+    //     ).subscribe(data => {
+    //       console.log("returned",data);
+    //       stepper.selected.completed = true;
+    //       stepper.next();
+    //     },
+    //     error => {
+    //       this.showToast("We konden de gegevens niet opslaan. Ben je nog verbonden?", 3000);
+    //       console.log("error at data request", error);
+    //     }
+    //   );
   }
 
-  async showToast(text: string, duration: number) {
+  async showToast(text: string) {
     const toast = await this.toastController.create({
       message: text,
-      duration: duration,
+      duration: 3000,
     });
     toast.present();
   }
