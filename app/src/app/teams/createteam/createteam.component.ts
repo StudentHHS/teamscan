cef8173f0d7b02e5ffded95e0d773d775d6b6466
Unknown <jhut98@gmail.com>
2019-11-28 22:58:41 +0100

    Create team: opleiding en samenwerkingsverband

--- app/src/app/teams/createteam/createteam.component.ts
+++ app/src/app/teams/createteam/createteam.component.ts
@@ -6,6 +6,19 @@ import { HttpClient } from '@angular/common/http';
 import { AuthService } from '../../auth.service';
 import { ToastController } from '@ionic/angular';
 import { Location } from '@angular/common';
+import { map, startWith } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+export interface OpleidingenGroup {
+  naam: string;
+  opleidingen: string[];
+}
+
+export const _filter = (opt: string[], value: string): string[] => {
+  const filterValue = value.toLowerCase();
+
+  return opt.filter(item => item.toLowerCase().indexOf(filterValue) >= 0);
+};
 
 @Component({
   selector: 'app-createteam',
@@ -26,12 +39,28 @@ export class CreateTeamComponent {
 
   constructor(private fb: FormBuilder, private location: Location, private authService: AuthService, private http: HttpClient, private toastController: ToastController) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.createTeamForm.get('samenwerking').valueChanges
+      .subscribe(value => {
+        console.log(value);
+        if (value === '1') {
+          this.createTeamForm.removeControl("opleidingEnFaculteit");
+        } else {
+          this.createTeamForm.addControl("opleidingEnFaculteit", new FormControl('', [Validators.required]));
+        }
+      });
+    this.stateGroupOptions = this.createTeamForm.get('opleidingEnFaculteit')!.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this._filterGroup(value))
+      );
+  }
 
   createTeamForm: FormGroup = this.fb.group({
     naam: ['', Validators.required],
     beschrijving: ['', Validators.required],
-    teamleider: ['', [Validators.required], [this.isValidUser.bind(this)]],
+    samenwerking: ['0',Validators.required],
+    opleidingEnFaculteit: '',
     teamleden: new FormArray([], [this.validateArrayNotEmpty])
   });
 
@@ -130,4 +159,85 @@ export class CreateTeamComponent {
       });
     }
   }
+
+  stateGroups: OpleidingenGroup[] = [{
+    naam: 'Faculteit Business, Finance & Marketing',
+    opleidingen: ['Accountancy', 'Finance & control', 'Commerciële Economie', 'International Business', 'Ondernemerschap & Retail Management', 'Kenniskring / Lectoraat', 'Faculteitsbureau'
+    ]
+  }, {
+    naam: 'Faculteit Bestuur, Recht & Veiligheid',
+    opleidingen: ['Bestuurskunde/Overheidsmanagement', 'HBO-Rechten', 'Integrale Veiligheidskunde', 'Kenniskring / Lectoraat', 'Faculteitsbureau'
+        ]
+  }, {
+    naam: 'Faculteit Gezondheid, Voeding & Sport',
+    opleidingen: ['Mens en Techniek | Bewegingstechnologie', 'Opleiding tot Leraar Lichamelijke Opvoeding', 'Opleiding tot Verpleegkundige','Huidtherapie','Voeding & Diëtetiek','Sportkunde (International Sportmanagement)','Kenniskring / Lectoraat','Faculteitsbureau']
+  }, {
+    naam: 'Faculteit IT & Design',
+    opleidingen: ['HBO-ICT - Den Haag','HBO-ICT - Delft','HBO-ICT - Zoetermeer','Communication and Multimedia Design','Kenniskring / Lectoraat','Faculteitsbureau']
+  }, {
+    naam: 'Faculteit Management & Organisatie',
+    opleidingen: ['Bedrijfskunde','Communicatie','European Studies','Facility Management','Human Resource Management','Kenniskring / Lectoraat','Faculteitsbureau']
+  }, {
+    naam: 'Faculteit Sociaal Werk & Educatie',
+    opleidingen: ['Opleiding tot Leraar Basisonderwijs','Pedagogiek','Social Work','Kenniskring / Lectoraat','Faculteitsbureau']
+  }, {
+    naam: 'Faculteit Technologie, Innovatie & Samenleving',
+    opleidingen: ['Bouwkunde','PFT | Process & Food Technology','Civiele Techniek','Elektrotechniek','IPO | Industrieel Product Ontwerpen','Mechatronica','Ruimtelijke Ontwikkeling | Climate & Management','Technische Bedrijfskunde','Technische Natuurkunde','Toegepaste Wiskunde','Werktuigbouwkunde','Kenniskring / Lectoraat','Faculteitsbureau']
+  }, {
+    naam: 'Academie voor Masters & Professional Courses',
+    opleidingen: []
+  }, {
+    naam: 'Bestuurszaken',
+    opleidingen: []
+  }, {
+    naam: 'Dienst Bedrijfsvoering & Control',
+    opleidingen: ['Dienstbureau','Unit Control','Unit Services','Unit Subsidiedesk']
+  }, {
+    naam: 'Dienst Facilitaire Zaken & IT',
+    opleidingen: ['Dienstbureau','Unit Facility Services & Huisvesting','Unit Frontoffice & Support','Unit Innovatie & Projecten','Unit IT & Educational Services']
+  }, {
+    naam: 'Dienst Human Resources Management',
+    opleidingen: ['Dienstbureau','Unit Strategie en HRD','Unit Advies en Dienstverlening']
+  }, {
+    naam: 'Dienst Onderwijs, Kennis & Communicatie',
+    opleidingen: ['Dienstbureau','Unit Bibliotheek','Unit Onderwijs & Onderzoek','Unit Studentenservice','Unit Wereldburgerschap & Internationalisering','Unit Marketing en Communicatie']
+  }
+];
+
+stateGroupOptions: Observable<OpleidingenGroup[]>;
+
+private _filterGroup(value: string): OpleidingenGroup[] {
+    if (value) {
+        return this.stateGroups
+        .map(group => ({naam: group.naam, opleidingen: _filter(group.opleidingen, value)}))
+        .filter(group => group.opleidingen.length > 0);
+    }
+
+    return this.stateGroups;
+}
+
+validateFaculteit(c: FormControl) {
+    var value = c.value.split(" — ");
+    if(!c.value) return null;
+    var opleiding = value[0];
+    var faculteit = value[1];
+  
+    function zoekFaculteit(e) { 
+        return e.naam === faculteit;
+    }
+
+    var faculteitsGroep = this.stateGroups.find(zoekFaculteit);
+    if(!faculteitsGroep) return {
+        validateFaculteit: {
+          valid: false
+        }
+      };
+    var index = faculteitsGroep.opleidingen.indexOf(opleiding);
+    console.log(index);
+    return index > -1 ? null : {
+      validateFaculteit: {
+        valid: false
+      }
+    };
+}
 }
